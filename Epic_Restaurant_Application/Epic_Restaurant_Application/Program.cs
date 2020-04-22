066237247b4ebc0f3b7bb9795aa43a619fb27431
Kristófer a <andersen19@gmail.com>
2020-04-22 16:32:59 +0200

    Just created the .net core console application
    Will continue to expand upon it and push small things that work

---
+++ Epic_Restaurant_Application/Epic_Restaurant_Application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Epic_Restaurant_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
